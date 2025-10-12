31ff24df5dfe342e57c7b3c5fc91702bee00d3e0
hassan <hassan@kuality.ai>
2025-10-12 21:04:35 +0300

    add ILibraryItem interface

---
+++ SVU_Homework/Interfaces/ILibraryItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVU_Homework.Interfaces
+{
+    public interface ILibraryItem
+    {
+        string Title { get; }
+        string Author { get; }
+        bool IsAvailable { get; }
+        void DisplayInfo();
+    }
+}
